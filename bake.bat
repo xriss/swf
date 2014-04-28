faf1974e1f537535fa153654b8c50298011a0683
kriss <kriss@localhost>
2014-04-28 15:26:26 +0100

    build and save the swfs in out

--- bake.bat
+++ bake.bat
@@ -1,71 +1,93 @@
 
-set BASECD=c:\wet\swf
+set BASECD=c:\Users\Kriss\swf
 
 cd %BASECD%\asue1
 call bake.bat
+copy out\*.swf %BASECD%\out
 
 cd %BASECD%\asue2
 call bake.bat
+copy out\*.swf %BASECD%\out
 
 cd %BASECD%\basement
 call bake.bat
+copy out\*.swf %BASECD%\out
 
 cd %BASECD%\batwsball
 call bake.bat
+copy out\*.swf %BASECD%\out
 
 cd %BASECD%\bowwow
 call bake.bat
+copy out\*.swf %BASECD%\out
 
 cd %BASECD%\diamonds
 call bake.bat
+copy out\*.swf %BASECD%\out
 
 cd %BASECD%\estension
 call bake.bat
+copy out\*.swf %BASECD%\out
 
 cd %BASECD%\gojirama
 call bake.bat
+copy out\*.swf %BASECD%\out
 
 cd %BASECD%\island
 call bake.bat
+copy out\*.swf %BASECD%\out
 
 cd %BASECD%\itsacoop
 call bake.bat
+copy out\*.swf %BASECD%\out
 
 cd %BASECD%\mute
 call bake.bat
+copy out\*.swf %BASECD%\out
 
 cd %BASECD%\only1
 call bake.bat
+copy out\*.swf %BASECD%\out
 
 cd %BASECD%\pief
 call bake.bat
+copy out\*.swf %BASECD%\out
 
 cd %BASECD%\pixlcoop
 call bake.bat
+copy out\*.swf %BASECD%\out
 
 cd %BASECD%\rgbtd
 call bake.bat
+copy out\*.swf %BASECD%\out
 
 cd %BASECD%\romzom
 call bake.bat
+copy out\*.swf %BASECD%\out
 
 cd %BASECD%\take1
 call bake.bat
+copy out\*.swf %BASECD%\out
 
 cd %BASECD%\ville
 call bake.bat
+copy out\*.swf %BASECD%\out
 
 cd %BASECD%\wetcell
 call bake.bat
+copy out\*.swf %BASECD%\out
 
 cd %BASECD%\wetdike
 call bake.bat
+copy out\*.swf %BASECD%\out
 
 cd %BASECD%\wetlinks
 call bake.bat
+copy out\*.swf %BASECD%\out
 
 cd %BASECD%\wetv
 call bake.bat
+copy out\*.swf %BASECD%\out
 
 
 cd %BASECD%
